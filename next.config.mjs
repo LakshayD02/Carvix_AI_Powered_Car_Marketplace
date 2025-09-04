46a46d8a0b76e1f4989620b70c04638c7a6d37ad
LAKSHAY DHOUNDIYAL <104713661+LakshayD02@users.noreply.github.com>
2025-09-04 18:52:19 +0530

    Carvix Deploy

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ymqpkygmownybanldbpq.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "znygiyidqgsbvovcnsfi.supabase.co", // ← added this one
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' ",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
